de9733a7d473744a1f353d4df3dc3c1370609baa
karangupta99 <jrkaran85@gmail.com>
2024-05-30 01:28:36 +0530

    name change of model file

---
+++ models/post-model.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose")
+const user = require("./user-model")
+
+const postSchema = mongoose.Schema({
+   user:{
+    type: mongoose.Schema.Types.ObjectId,
+    ref:"user"
+   },
+   
+   content:String,
+   likes:[{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:"user"
+   }]
+  
+})
+
+module.exports = mongoose.model("post",postSchema)
---
+++ models/user-model.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose")
+
+mongoose.connect("mongodb://127.0.0.1:27017/mini")
+
+const userSchema = mongoose.Schema({
+    name:String,
+    username:String,
+    email:String,
+    password:String,
+   
+    posts:[{
+        type:mongoose.Schema.Types.ObjectId,ref:"post"},
+    ]  
+})
+
+module.exports = mongoose.model("user",userSchema)
